6069d84d29464f63e697e1403fe36475d23c9c8d
Osama Hassanein <player1os@gmail.com>
2017-03-01 16:30:11 +0100

    Add module for handling recursive promise loops

---
+++ src/common/promise_loop.js
@@ -0,0 +1,19 @@
+// The initial value and all subsequent values are feeded to the generator callback.
+// The generator returns a promise, which resolves to a { Boolean: done, any value } object.
+// When the promise is resolved the 'done' property of the result is checked.
+// If true the 'value' property is returned as the result of the promise loop.
+// Otherwise the function is called recursively with the 'value' property as the new initial value.
+const promiseLoop = (initialValue, generator) => {
+	return generator(initialValue)
+		.then(({
+			done,
+			value,
+		}) => {
+			return done
+				? value
+				: promiseLoop(value, generator);
+		});
+};
+
+// Expose the recursive loop function.
+export default promiseLoop;
