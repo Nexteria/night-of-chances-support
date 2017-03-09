bfc8093ebfed8df75ab1e5e49334f3ea741a3aeb
Osama Hassanein <player1os@gmail.com>
2017-03-09 10:14:27 +0100

    Add comment to the express promise module

--- src/server/lib/express_promise.js
+++ src/server/lib/express_promise.js
@@ -1,3 +1,9 @@
+// Converts a promise into a callback that is acceptable by express.
+// Depending on the result of the promise the following outcomes may occur:
+// - true: the next middleware (passed in the same router call) is executed.
+// - false: the next middleware (passed in the following router call) is executed.
+// - anything else: no middleware is executed.
+// If an error is thrown within the promise the error middleware is executed.
 export default (promise) => {
 	return (req, res, next) => {
 		promise
