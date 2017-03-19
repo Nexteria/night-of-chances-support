74bad634dd9d67dd32d4d2cd71d60f8bd1d381b3
Osama Hassanein <player1os@gmail.com>
2017-03-19 16:26:39 +0100

    Add optional debug logger

--- src/server/app/index.js
+++ src/server/app/index.js
@@ -2,6 +2,7 @@
 import apiMiddleware from '@/src/server/app/middleware/api';
 import errorMiddleware from '@/src/server/app/middleware/error';
 import frontendMiddleware from '@/src/server/app/middleware/frontend';
+import config from '@/src/server/lib/config';
 import * as paths from '@/src/server/lib/paths';
 
 // Load npm modules.
@@ -13,6 +14,24 @@ const app = express();
 
 // Parse application/json bodies.
 app.use(bodyParser.json());
+
+// Enable logger if set.
+if (config.APP_HTTP_ENABLE_LOGGER) {
+	app.use((req, res, next) => {
+		// Log request data.
+		console.log({ // eslint-disable-line no-console
+			ip_address: req.ip,
+			http_version: req.httpVersion,
+			http_method: req.method,
+			url_path: req.originalUrl,
+			headers: req.headers,
+			parsed_body: req.body,
+		});
+
+		// Continue execution.
+		next();
+	});
+}
 
 // Add serving of static content.
 app.use(express.static(paths.buildBrowser));
