ebbca7bc75913983c11da587ab90575fc274295c
Osama Hassanein <player1os@gmail.com>
2017-03-19 10:53:31 +0100

    Finalize universal api route generator

--- src/server/lib/base_api_middleware.js
+++ src/server/lib/base_api_middleware.js
@@ -1,64 +1,96 @@
 // Load app modules.
 import expressPromise from '@/src/server/lib/express_promise';
+import {
+	EntityExists as EntityExistsError,
+	EntityNotFound as EntityNotFoundError,
+	Validation as ValidationError,
+} from '@/src/common/error';
 
 // Load npm modules.
 import {
 	Router as expressRouter,
 } from 'express';
+import httpStatus from 'http-status';
+
+const appendRoutes = (router, model) => {
+	router
+		.post(expressPromise((req, res) => {
+			return model.create(req.body)
+				.then((result) => {
+					res.status(httpStatus.CREATED).send(result);
+				})
+				.catch(ValidationError, EntityExistsError, (err) => {
+					res.status(httpStatus.BAD_REQUEST).send(err.details);
+				});
+		}))
+		.get(expressPromise((req, res) => {
+			return model.find(req.query)
+				.then((result) => {
+					res.status(httpStatus.OK).send(result);
+				})
+				.catch(ValidationError, (err) => {
+					res.status(httpStatus.BAD_REQUEST).send(err.details);
+				});
+		}))
+		.get('/:key', expressPromise((req, res) => {
+			return model.findByKey(req.params.key)
+				.then((result) => {
+					res.status(httpStatus.OK).send(result);
+				})
+				.catch(ValidationError, (err) => {
+					res.status(httpStatus.BAD_REQUEST).send(err.details);
+				})
+				.catch(EntityNotFoundError, () => {
+					res.status(httpStatus.NOT_FOUND).send();
+				});
+		}))
+		.put(expressPromise((req, res) => {
+			return model.update(req.query, req.body)
+				.then((result) => {
+					res.status(httpStatus.OK).send(result);
+				})
+				.catch(ValidationError, (err) => {
+					res.status(httpStatus.BAD_REQUEST).send(err.details);
+				});
+		}))
+		.put('/:key', expressPromise((req, res) => {
+			return model.update({
+				[model.primaryKeyField.name]: req.params.key,
+			}, req.body)
+				.then((result) => {
+					res.status(httpStatus.OK).send(result);
+				})
+				.catch(ValidationError, (err) => {
+					res.status(httpStatus.BAD_REQUEST).send(err.details);
+				});
+		}))
+		.delete(expressPromise((req, res) => {
+			return model.destroy(req.query)
+				.then((result) => {
+					res.status(httpStatus.OK).send(result);
+				})
+				.catch(ValidationError, (err) => {
+					res.status(httpStatus.BAD_REQUEST).send(err.details);
+				});
+		}))
+		.delete('/:key', expressPromise((req, res) => {
+			return model.destroy({
+				[model.primaryKeyField.name]: req.params.key,
+			})
+				.then((result) => {
+					res.status(httpStatus.OK).send(result);
+				})
+				.catch(ValidationError, (err) => {
+					res.status(httpStatus.BAD_REQUEST).send(err.details);
+				});
+		}));
+};
 
 export default (model) => {
 	const router = expressRouter();
 
-	router.post(expressPromise((req, res) => {
-
-	}));
-
-	router.get(expressPromise((req, res) => {
-
-	}));
-
-	router.get('/:key', expressPromise((req, res) => {
-
-	}));
-
-	router.put(expressPromise((req, res) => {
-
-	}));
-
-	router.put('/:key', expressPromise((req, res) => {
-
-	}));
-
-	router.delete(expressPromise((req, res) => {
-
-	}));
-
-	router.delete('/:key', expressPromise((req, res) => {
-
-	}));
-
-	router.route('/attribute')
-		.post(expressPromise((req, res) => {
-
-		}))
-		.get(expressPromise((req, res) => {
-
-		}))
-		.get('/:key', expressPromise((req, res) => {
-
-		}))
-		.put(expressPromise((req, res) => {
-
-		}))
-		.put('/:key', expressPromise((req, res) => {
-
-		}))
-		.delete(expressPromise((req, res) => {
-
-		}))
-		.delete('/:key', expressPromise((req, res) => {
-
-		}));
+	appendRoutes(router, model);
+	appendRoutes(router.route('/attribute'), model.attributeModel);
 
 	return router;
 };
