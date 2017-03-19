270370b527e2a91d0cbb135344c991aacee4619a
Osama Hassanein <player1os@gmail.com>
2017-03-19 16:27:09 +0100

    Remove unneeded modules

--- src/server/app/middleware/api/index.js
+++ src/server/app/middleware/api/index.js
@@ -1,8 +1,6 @@
-/* eslint-disable no-console */
-
 // Load app modules.
-import baseApiMiddleware from '@/src/server/lib/base_api_middleware';
-import * as expressRequest from '@/src/server/lib/express_request';
+import importMiddleware from '@/src/server/app/middleware/api/import';
+import baseApiMiddleware from '@/src/server/app/lib/base_api_middleware';
 import attendeeModel from '@/src/server/model/attendee';
 import eventModel from '@/src/server/model/event';
 import projectModel from '@/src/server/model/project';
@@ -15,24 +13,13 @@ import httpStatus from 'http-status';
 
 const router = expressRouter();
 
+router.use('/import', importMiddleware);
 router.use('/attendee', baseApiMiddleware(attendeeModel));
 router.use('/event', baseApiMiddleware(eventModel));
 router.use('/project', baseApiMiddleware(projectModel));
 
 router.use((req, res) => {
 	res.status(httpStatus.NOT_FOUND).send('Not Found');
-});
-
-// Define basic behaviour.
-router.use((req, res) => {
-	console.log(expressRequest.getIpAddress(req));
-	console.log(req.httpVersion);
-	console.log(req.method);
-	console.log(expressRequest.getFullUrl(req));
-	console.log(req.headers);
-	console.log(req.rawBody);
-
-	res.status(httpStatus.OK).send();
 });
 
 export default router;
